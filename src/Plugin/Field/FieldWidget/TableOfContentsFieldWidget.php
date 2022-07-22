6a25c7acb6443725b8d3b22c9398c99a1bd89a06
Roger Gee <roger.gee@tulsalibrary.org>
2022-07-22 17:00:08 -0500

    Move formatter settings from widget to formatter

--- src/Plugin/Field/FieldWidget/TableOfContentsFieldWidget.php
+++ src/Plugin/Field/FieldWidget/TableOfContentsFieldWidget.php
@@ -66,23 +66,9 @@ class TableOfContentsFieldWidget extends WidgetBase {
     $settings = parent::defaultSettings();
 
     $settings += [
-      // List of field types that are searched for headings.
-      'field_types' => [
-        'text_long',
-        'text_with_summary',
-      ],
-
-      // A list of "bundle:field_name" strings that are interpreted as a pure
-      // heading. The type of these fields need not exist in 'field_types'.
-      'heading_fields' => [],
-
       // The list of node bundle types allowed for entities assigned to the
       // table of contents field.
       'node_bundles' => ['page','article'],
-
-      // If a 'paragraph' entity revision field is found, the table of contents
-      // will recurse through any child paragraph fields.
-      'scan_paragraphs' => true,
     ];
 
     return $settings;
@@ -94,85 +80,12 @@ class TableOfContentsFieldWidget extends WidgetBase {
   public function settingsForm(array $form,FormStateInterface $form_state) {
     $form = parent::settingsForm($form,$form_state);
 
-    $fieldTypePluginManager = \Drupal::service('plugin.manager.field.field_type');
-    $fieldTypeOptions = [];
-    foreach ($fieldTypePluginManager->getDefinitions() as $name => $info) {
-      $fieldTypeOptions[$name] = $info['label'] ?? $name;
-    }
-
-    $form['field_types'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Supported Field Types'),
-      '#default_value' => $this->getSetting('field_types'),
-      '#description' => $this->t(
-        'One or more field type IDs that are searched for headings from which '
-        .'to generate the table of contents.'
-      ),
-      '#options' => $fieldTypeOptions,
-      '#multiple' => true,
-      '#sort_options' => true,
-      '#attributes' => [
-        'style' => 'width: 70ch;height: 20em',
-      ],
-    ];
-
     $bundleOptions = [];
     $bundleService = \Drupal::service('entity_type.bundle.info');
     $bundles = $bundleService->getBundleInfo('node');
     foreach ($bundles as $name => $bundle) {
       $bundleOptions[$name] = $bundle['label'] ?? $name;
     }
-
-    $entityFieldManager = \Drupal::service('entity_field.manager');
-    $moduleHandler = \Drupal::service('module_handler');
-    $headingFieldOptions = [];
-    foreach ($bundles as $bundle => $bundleInfo) {
-      $innerOptions = [];
-
-      foreach ($entityFieldManager->getFieldDefinitions('node',$bundle)
-               as $fieldName => $def)
-      {
-        if (substr($fieldName,0,strlen('field_')) != 'field_') {
-          continue;
-        }
-
-        $innerOptions["node:$bundle:$fieldName"] = $def->getLabel();
-      }
-
-      if (!empty($innerOptions)) {
-        $headingFieldOptions[$bundleInfo['label'] ?? $bundle] = $innerOptions;
-      }
-    }
-    if ($moduleHandler->moduleExists('paragraphs')) {
-      $paragraphBundles = $bundleService->getBundleInfo('paragraph');
-      foreach ($paragraphBundles as $bundle => $bundleInfo) {
-        $defs = $entityFieldManager->getFieldDefinitions('paragraph',$bundle);
-        foreach ($defs as $fieldName => $def) {
-          if (substr($fieldName,0,strlen('field_')) != 'field_') {
-            continue;
-          }
-
-          $label = "Paragraphs Module ({$bundleInfo['label']})";
-          $headingFieldOptions[$label]["paragraph:$bundle:$fieldName"] = $def->getLabel();
-        }
-      }
-    }
-
-    $form['heading_fields'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Heading Fields'),
-      '#default_value' => $this->getSetting('heading_fields'),
-      '#description' => $this->t(
-        'Zero or more field names that are to be interpreted as headings. '
-        .'Note that the field type is not considered for heading fields.'
-      ),
-      '#options' => $headingFieldOptions,
-      '#multiple' => true,
-      '#sort_options' => true,
-      '#attributes' => [
-        'style' => 'width: 70ch;height: 20em',
-      ],
-    ];
 
     $form['node_bundles'] = [
       '#type' => 'select',
@@ -188,17 +101,6 @@ class TableOfContentsFieldWidget extends WidgetBase {
       '#attributes' => [
         'style' => 'width: 70ch;height: 20em',
       ],
-    ];
-
-    $form['scan_paragraphs'] = [
-      '#type' => 'checkbox',
-      '#return_value' => 1,
-      '#title' => 'Scan Paragraphs',
-      '#default_value' => $this->getSetting('scan_paragraphs'),
-      '#description' => $this->t(
-        'Toggles scanning of Paragraph field instances when generating the '
-        .'table of contents.'
-      ),
     ];
 
     return $form;
